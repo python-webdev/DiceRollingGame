cb3d21a92152b6e36718c7e8a6c84b7247b7a43c
Murod Akhmedov <python_webdev@outlook.com>
2026-02-13 04:49:33 +0000

    Add dice rolling game with user input loop

---
+++ dice_rolling_game.py
@@ -0,0 +1,23 @@
+# Loop inside:
+#   Ask: roll the dice?
+#   If user enters y
+#     Generate two random numbers between 1 and 6
+#     Print them
+#   If user enters n
+#     Print "Goodbye!"
+#     Terminate the program
+#   Else
+#     Print "Invalid input. Please enter 'y' or 'n'."
+import random
+
+while True:
+    user_input = input('Roll the dice? (y/n): ').lower()
+    if user_input == 'y':
+        dice1 = random.randint(1, 6)
+        dice2 = random.randint(1, 6)
+        print(f'You rolled: {dice1} and {dice2}')
+    elif user_input == 'n':
+        print('Goodbye!')
+        break
+    else:
+        print("Invalid input. Please enter 'y' or 'n'.")
