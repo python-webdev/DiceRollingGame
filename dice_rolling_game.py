ebe091bb155344288fd6efae01156f2fdb4e79af
Murod Akhmedov <webdevlondon@outlook.com>
2026-02-14 20:16:22 +0000

    Add total score and rules

--- dice_rolling_game.py
+++ dice_rolling_game.py
@@ -1,5 +1,5 @@
 import random
-
+# Add total score.
 roll_count = 0
 while True:
     user_input = input('Roll the dice? (y/n): ').lower()
@@ -15,12 +15,26 @@ while True:
             print('Dice count must be at least 2. Please try again.')
             continue
         dice_rolls = [random.randint(1, 6) for _ in range(num_dice)]
-        dice1, dice2 = dice_rolls[:2]  # Get the first two dice rolls
-        print(f'You rolled: {dice1} and {dice2}')
+        total = sum(dice_rolls)
+        rolled_numbers = ", ".join(map(str, dice_rolls))
+        # Check if all dice show the same number (doubles)
+        has_doubles = len(set(dice_rolls)) == 1
+        if has_doubles:  # All dice show the same number
+            print(
+                f'\nYou rolled: {rolled_numbers} (Doubles! You get an extra turn!)')
+            continue  # Allow the player to roll again immediately
+        if total > 10:
+            print(
+                f'\nYou rolled: {rolled_numbers} (Congratulations! You win!)')
+        elif total < 5:
+            print(
+                f'\nYou rolled: {rolled_numbers} (Sorry, you lose!)')
+        else:
+            print(
+                f'\nYou rolled: {rolled_numbers} (It\'s a draw!)')
+        print(f'Total score: {total}')
         roll_count += 1
-        if dice1 == dice2:
-            print('Congratulations! You rolled doubles!')
-        print(f'You have rolled the dice {roll_count} times.')
+        print(f'You have rolled the dice {roll_count} times. \n')
     elif user_input == 'n':
         print('Thank you for playing! Goodbye!')
         break
